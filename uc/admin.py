ca1c2197c8e60e36635f54f91ae85c84596e9cfa
Leandro Pereira <leandro.escrevame@gmail.com>
2021-11-20 19:36:56 -0300

    Desenvolvimento de base dados para UCs e registros de atividades

--- uc/admin.py
+++ uc/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UC, AtividadeUC
+
+admin.site.register(UC)
+admin.site.register(AtividadeUC)
+
 
 # Register your models here.
--- uc/models.py
+++ uc/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class UC(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    thumb=models.ImageField(upload_to ="thumb_uc")
+
+    def __str__(self) -> str:
+        return self.nome
+
+class AtividadeUC(models.Model):
+    nome_atividade = models.CharField(max_length=100)
+    descricao = models.TextField()
+    plano = models.FileField(upload_to = "plano")
+    complemento_1= models.FileField (upload_to = "complemento")
+    uc = models.ForeignKey(UC, on_delete = models.DO_NOTHING)
+
+    def __str__(self) -> str:
+        return self.nome_atividade
